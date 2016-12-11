d9e53d760418632ddfb7664a87ae746c514ffde0
Mohamed Riza <riza.mohammed@hotmail.com>
2016-12-11 14:56:35 -0500

    Added Event ViewModels - Variables not yet defined

---
+++ GCETNChapter/GCETNChapter/Models/ViewModels/Events/EventGalleryVO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GCETNChapter.Models.ViewModels.Events
+{
+    public class EventGalleryVO
+    {
+    }
+}
---
+++ GCETNChapter/GCETNChapter/Models/ViewModels/Events/EventsVO.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GCETNChapter.Models.ViewModels.Events
+{
+    public class EventsVO
+    {
+        [Display(Name = "Event ID")]
+        public int EventID { get; set; }
+
+        [Display(Name = "* Event Name")]
+        public string EventName { get; set; }
+
+        [Display(Name = "* Start Date")]
+        public DateTime StartDate { get; set; }
+
+        [Display(Name = "* End Date")]
+        public DateTime EndDate { get; set; }
+
+        [Display(Name = "Total Collected Amount")]
+        public decimal TotalCollectedAmount { get; set; }
+
+        [Display(Name = "Total Expense Amount")]
+        public decimal TotalExpenseAmount { get; set; }
+
+        [Display(Name = "Created By")]
+        public string CreatedBy { get; set; }
+
+    }
+}
